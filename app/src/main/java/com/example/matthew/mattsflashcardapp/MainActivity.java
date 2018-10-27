4f1c7ca06262c0a3643ee4212df3346a6842f664
mjr445 <43094069+mjr445@users.noreply.github.com>
2018-10-27 19:38:13 -0400

    New additions for Lab 2

--- app/src/main/java/com/example/matthew/mattsflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/matthew/mattsflashcardapp/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.matthew.mattsflashcardapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView flashcardQ;
+
+    TextView flashcardA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flashcardQ = findViewById(R.id.flashcard_question);
+        flashcardA = findViewById(R.id.flashcard_answer);
 
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -18,6 +27,24 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
             }
         });
+
+        findViewById(R.id.PlusButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100 && resultCode == RESULT_OK) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            String string1 = data.getExtras().getString("string1"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String string2 = data.getExtras().getString("string2");
+            flashcardQ.setText(string1);
+            flashcardA.setText(string2);
+        }
     }
 
 
